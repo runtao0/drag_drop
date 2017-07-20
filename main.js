10e25db4b045be163aa78d3c128df8ccaa89a00e
Runtao0 <ry2228@columbia.edu>
2017-07-19 23:45:38 -0400

    drag and drop almost complete

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(() => {
     const container = $('body');
     const userIDs = [ 1, 2 ];
     const model = 'users';
+
+    // make this mmore modular
     const associated = "albums";
     const table1 = new Table({ container });
 
@@ -14,67 +16,21 @@ $(() => {
         )
     })
 
-
     $.when(...deferred).then((table) => {
+        table.render();
+        table.addDragEvents();
         console.log(table1);
-        debugger
     })
 
-
-    userIDs.forEach((id) => {
-        jsonCall({ model, id })
-        .then(handleUser(container))
-        .then((albumList) => {
-            return jsonCall({ model, id, associated })
-            .then(handleAlbums(albumList))
-        })
-        .then((albumList) => {
-            albumList.mousedown((e) => {
-                const startX = e.clientX;
-                const startY = e.clientY;
-
-                const li = $(e.target).closest("li")[0];
-                debugger
-            })
-        })
-    })
 })
 
-
-const jsonCall = ({ model, id, associated, method }) => {
-    return $.ajax({
-        url: `https://jsonplaceholder.typicode.com/${model}/${id}/${associated ? associated : ''}`,
-        method: method ? method : 'GET'
-    })
-}
-
-const handleUser = (container) => (user) => {
-    const name = user.name ? user.name : "No name";
-    const section = $("<section>", { "class": "user-section", id: user.id });
-    container.append(section);
-    const albumList = $("<ul>", { "class": "album-list"});
-    section.append(_userHeader(name))
-        .append(albumList);
-
-    return albumList;
-}
-
-const handleAlbums = (albumList) => (albums) => {
-    albums.forEach(_addAlbumToList(albumList));
-    return albumList;
-}
-
-const _addAlbumToList = (albumList) => (album) => {
-    albumList.append(_albumListItem(album))
+const _albumList = (userID) => {
+    return $("<ul>", { "class": "album-list", id: `${userID}-user`});
 }
 
 const _albumListItem = (album) => {
-    return $("<li>", { id: album.id, draggable: true }).append(_albumID(album.id))
+    return $("<li>", { id: album.id }).append(_albumID(album.id))
         .append(_albumTitle(album.title));
-}
-
-const _onDragStart = (first, second, third) => {
-    debugger
 }
 
 const _albumID = (id) => {
@@ -89,21 +45,40 @@ const _userHeader = (name) => {
     return $("<h2>", { "class": "name"}).text(`${name}`);
 }
 
+const _legend = () => {
+    return $("<article>").append($("<div>").text("id"))
+        .append($("<div>").text("title"));
+}
+
 class Table {
     constructor({ container }) {
         this.container = container;
         this.state = {};
         this.recordUser = this.recordUser.bind(this);
         // this.recordAlbums = this.recordAlbums.bind(this);
+        this._renderUsers = this._renderUsers.bind(this);
+        this._replaceAlbums = this._replaceAlbums.bind(this);
+        this._moveListItem = this._moveListItem.bind(this);
     }
 
-    fetchModel({ model, id, associated, method }) {
+    fetchModel({ model, id, associated }) {
         return $.ajax({
             url: `https://jsonplaceholder.typicode.com/${model}/${id}/${associated ? associated : ''}`,
-            method: method ? method : 'GET'
+            method: 'GET'
         })
     }
-    //refactor this to look like recordAlbums
+
+    _patchModel({ userId, id }) {
+        debugger
+        return $.ajax({
+            url: `https://jsonplaceholder.typicode.com/albums/${id}/`,
+            method: 'PATCH',
+            data: {
+                userId
+            }
+        })
+    }
+
     recordUser(user) {
         const newState = {};
         newState[user.id] = user
@@ -111,9 +86,6 @@ class Table {
         return this;
     }
 
-    // recordUser({ model, id, associated, method }) {
-    //     return new Promise
-    // }
 
     _storeAlbums(albums) {
         albums.forEach((album) => {
@@ -123,10 +95,8 @@ class Table {
             } else {
                 if (!this.state[album.userId].albums) {
                     this.state[album.userId].albums = {};
-                    this.state[album.userId].albums[album.id] = album;
-                } else {
-                    this.state[album.userId].albums[album.id] = album;
                 }
+                    this.state[album.userId].albums[album.id] = album;
             }
         });
         return this;
@@ -140,19 +110,109 @@ class Table {
 
     }
 
-    // renderUsers() {
-    //     const userIDs = Object.keys(this.state)
-    //     const name = user.name ? user.name : "No name";
-    //     const section = $("<section>", { "class": "user-section", id: user.id });
-    // }
-    // const handleUser = (container) => (user) => {
-    //
-    //     container.append(section);
-    //     const albumList = $("<ul>", { "class": "album-list"});
-    //     section.append(_userHeader(name))
-    //         .append(albumList);
-    //
-    //     return albumList;
-    // }
+    render() {
+        this._renderUsers()
+        this._replaceAlbums()
+    }
+
+    //not pure
+    _renderUsers() {
+        for (let userID in this.state) {
+            const name = this.state[userID].name ? this.state[userID].name : "No name";
+            const section = $("<section>", { "class": "user-section"});
+            this.state[userID].list = _albumList(userID);
+            section.append(_userHeader(name))
+                .append(_legend())
+                .append(this.state[userID].list);
+
+            this.container.append(section);
+        }
+    }
+
+    _replaceAlbums() {
+        for (let userID in this.state) {
+            for (let albumID in this.state[userID].albums) {
+                const listItem = _albumListItem(this.state[userID].albums[albumID])
+                this.state[userID].list.append(listItem);
+                this.state[userID].albums[albumID].element = listItem;
+            }
+        }
+    }
+
+    addDragEvents() {
+        for (let userID in this.state) {
+            if (!this.state[userID].list) alert("cannot add drag");
+            this.state[userID].list.mousedown((e) => {
+                const section = $(e.target).closest("section")
+                const li = $(e.target).closest("li");
+                const parent = li.parent();
+                if (!li) return;
+
+                section.css('z-index', "1")
+                li.css('z-index', '5');
+
+                const initialLiPosition = li.offset();
+                const initialMouseX = e.clientX;
+                const initialMouseY = e.clientY;
+
+                document.onmousemove = this._onMouseMove({ li, initialMouseX, initialMouseY, initialLiPosition })
+                document.onmouseup = this._onMouseUp({ li, section, initialLiPosition, parent })
+            })
+        }
+    }
+
+    _onMouseMove({ li, initialMouseX, initialMouseY, initialLiPosition }) {
+        return (e2) => {
+            const currentMouseX = e2.clientX;
+            const currentMouseY = e2.clientY;
+
+            li.offset({
+                left: ((currentMouseX - initialMouseX) + initialLiPosition.left),
+                top: ((currentMouseY - initialMouseY) + initialLiPosition.top)
+            })
+        }
+    }
+
+    _onMouseUp({ li, section, initialLiPosition, parent }) {
+        return (e3) => {
+            let returnToOrigin = true;
+            const mouseUpX = e3.clientX;
+            const mouseUpY = e3.clientY;
+            $(".album-list").each((index, albumList) => {
+                const $albumList = $(albumList);
+                const albumPosition = $albumList.offset();
+                if ((mouseUpX > albumPosition.left && mouseUpX < (albumPosition.left + $albumList.width())) &&
+                    (mouseUpY > albumPosition.top && mouseUpY < albumPosition.top + $albumList.height())) {
+                        if (albumList !== parent[0]) {
+                            returnToOrigin = false;
+                            $.when(this._patchModel({
+                                userId: parseInt(albumList.id, 10),
+                                id: li[0].id
+                            })).then((x) => {
+                                    debugger
+                                })
+                            //landed in the other list
+                        }
+                } else {
+                    return;
+                }
+            })
+            if (returnToOrigin) li.offset(initialLiPosition);
+            document.onmousemove = null
+            section.css('z-index', "0")
+        }
+    }
+
+    _moveListItem(item) {
+        // removefromoldcolumn
+        // addtoNewColumn
+    }
+
+
+
+    _removeFromOldColumn({ parent, target, }){
+        // remove from state
+        //and remove from ul
+
+    }
 }
-// const newSection = ({id})
